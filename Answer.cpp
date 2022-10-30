daba023227b5600761590b4916daadc57803e125
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-10-30 11:59:37 +0530

    Add files via uploading

---
+++ Answer.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    int samp[1000000]={0};
+	    int count=0;
+	    
+	    for (int i=0;i<n;i++){
+	        samp[arr[i]]=1;
+	    }
+	    
+	    for(int i=0;i<n-1;i++){
+	        for(int j=i+1;j<n;j++){
+	            if(samp[arr[i]+arr[j]]==1){
+	                count++;
+	            }
+	        }
+	    }
+	    return count;
+	}
+};
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        	cin >> arr[i];
+
+        Solution ob;
+        cout << ob.countTriplet(arr, n) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
